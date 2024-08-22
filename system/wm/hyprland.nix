c744d9e11f1d14d636c557728eed4809a7186719
hyperboly <johnwuonmail@gmail.com>
2024-08-22 16:58:26 +0800

    updated hyprland input

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -4,8 +4,8 @@
     programs = {
         hyprland = {
             enable = true;
-            package = pkgs.hyprland;
-            #package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+            #package = pkgs.hyprland;
+            package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
         };
     };
 }
