7ac25f6a70835080152069abc26cf2bd4c0c788e
hyperboly <johnwuonmail@gmail.com>
2025-09-01 00:56:09 +0800

    starting my win10 bar

--- user/wm/niri/niri.nix
+++ user/wm/niri/niri.nix
@@ -2,5 +2,11 @@
 
 {
   home.file.".config/niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/user/wm/niri/config.kdl";
-  programs.swaylock.enable = true;
+  programs = {
+    swaylock.enable = true;
+    eww = {
+      enable = true;
+      configDir = ./eww;
+    };
+  };
 }
