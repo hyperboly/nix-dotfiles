9ca886dfdd012100c1550eda992214716787d6e6
hyperboly <johnwuonmail@gmail.com>
2025-09-18 17:12:52 +0800

    fix lazygit

--- user/neovim/conf/lua/kickstart/plugins/lazygit.lua
+++ user/neovim/conf/lua/kickstart/plugins/lazygit.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require('lazygit-nvim').setup()
+-- require('lazygit-nvim').setup()
 
 -- Document existing key chains
 -- require('lazygit-nvim').add {
