9a6e6713445a36cfc992da51d7ee256b48b8a674
hyperboly <johnwuonmail@gmail.com>
2026-01-15 20:54:28 +0800

    changing notificatiosn

--- user/notifications/scripts/alert.sh
+++ user/notifications/scripts/alert.sh
@@ -4,5 +4,5 @@ if [[ "$DUNST_SUMMARY" =~ ^\[SYSTEM\] ]]; then
     #aplay ~/.dotfiles/user/notifications/audio/animegirl.wav
     return 0
 else
-    aplay ~/.dotfiles/user/notifications/audio/youGotmail.wav
+    mpv ~/.dotfiles/user/notifications/audio/youGotmail.wav
 fi
