12fbc9043b0836988123fa519ab3692ea7807e30
hyperboly <johnwuonmail@gmail.com>
2026-01-15 21:23:46 +0800

    remove eww

--- user/wm/niri/niri.nix
+++ user/wm/niri/niri.nix
@@ -4,9 +4,9 @@
   home.file.".config/niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/user/wm/niri/config.kdl";
   programs = {
     swaylock.enable = true;
-    eww = {
-      enable = true;
-      configDir = ./eww-winbar;
-    };
+    # eww = {
+    #   enable = true;
+    #   configDir = ./eww-winbar;
+    # };
   };
 }
