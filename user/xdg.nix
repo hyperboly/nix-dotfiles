d5fd4da5345335c149f1cc3f19c18dc9025fc5c0
hyperboly <johnwuonmail@gmail.com>
2026-03-30 17:57:11 +0800

    added ardour8 low latency desktop profile

--- user/xdg.nix
+++ user/xdg.nix
@@ -24,9 +24,33 @@
             "image/gif" = "swayimg.desktop";
             "inode/directory" = "nemo.desktop";
             "inode/mount-point" = "yazi.desktop";
-            "video/*" = "mpv.desktop";
-            "audio/*" = "mpv.desktop";
+            "video/mpeg" = "mpv.desktop";
+            "video/mp4" = "mpv.desktop";
+            "video/x-m4v" = "mpv.desktop";
+            "video/avi" = "mpv.desktop";
+            "video/x-msvideo" = "mpv.desktop";
+            "video/ogg" = "mpv.desktop";
+            "video/webm" = "mpv.desktop";
+            "video/x-matroska" = "mpv.desktop";
+            "audio/mpeg" = "mpv.desktop";
+            "audio/x-mp3" = "mpv.desktop";
+            "audio/x-flac" = "mpv.desktop";
+            "audio/ogg" = "mpv.desktop";
+            "audio/aac" = "mpv.desktop";
         };
         mimeApps.enable = true;
+        desktopEntries = {
+            ardour = {
+                name = "Ardour (low latency)";
+                genericName = "Digital Audio Workstation";
+                comment = "Digital Audio Workstation";
+                exec = "env PIPEWIRE_LATENCY=128/48000 ardour8 %F";
+                icon = "ardour8";
+                terminal = false;
+                categories = [ "AudioVideo" "Audio" "Recorder" ];
+                mimeType = [ "application/x-ardour" ];
+            };
+
+        };
     };
 }
