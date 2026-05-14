41c818765a54dc29fa1b414e21df4f66f0c07181
hyperboly <johnwuonmail@gmail.com>
2026-05-14 10:25:40 +0800

    add: predictable interface names and changed FW rules

--- system/networking.nix
+++ system/networking.nix
@@ -3,15 +3,17 @@
 {
   networking = {
     hostName = systemSettings.hostname;
+    useDHCP = lib.mkDefault true;
+    usePredictableInterfaceNames = true;
     networkmanager = {
       enable = true;
       wifi.macAddress = "random";
     };
     nameservers = [ "100.100.100.100" ];
-    firewall = rec {
-      allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-      allowedUDPPortRanges = allowedTCPPortRanges;
-    };
+    # firewall = rec {
+    #   allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    #   allowedUDPPortRanges = allowedTCPPortRanges;
+    # };
     hostId = "eb3b649e";
 
     # Bridging
